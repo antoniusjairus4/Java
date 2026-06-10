9719375c7bfe4735f2b77331bc263bae1bf7e35d
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-06-10 14:23:01 +0530

    printing with OOP commit -1

--- Basic Problems/printing_with_OOP.java
+++ Basic Problems/printing_with_OOP.java
@@ -1,5 +1,18 @@
 import java.util.Scanner;
 
 class Patient{
-    
+    String name;
+    int age;
+    char gender;
+    boolean insurance;
+    double fee;
+
+    void display(){
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(gender);
+        System.out.println(insurance);
+        System.out.println(fee);
+    }
 }
+
