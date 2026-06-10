70e62e0a4630c02c54729b16ba35b900803cb538
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-06-10 14:33:50 +0530

    printing with OOP exercise

--- Basic Problems/printing_with_OOP.java
+++ Basic Problems/printing_with_OOP.java
@@ -16,3 +16,28 @@ class Patient{
     }
 }
 
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner (System.in);
+        Patient p = new Patient();
+
+        System.out.print("Enter the name of the patient : ");
+        p.name = s.nextLine();
+
+        System.out.print("Enter the age of the patient : ");
+        p.age = s.nextInt();
+
+        System.out.print("Enter the gender of the patient : ");
+        p.gender = s.next().charAt(0);
+
+        System.out.print("Enter the insutance status of the patient : ");
+        p.insurance = s.nextBoolean();
+
+        System.out.print("Enter the fee of the patient : ");
+        p.fee = s.nextDouble();
+
+        p.display();
+        s.close();
+    }
+}
+
