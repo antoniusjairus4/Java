ef8a2042fb2efce4b043f20938eb1eda5810d6c0
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-06-10 11:47:30 +0530

    Creating folders and organising

--- Basic Problems/Main.java
+++ Basic Problems/Main.java
@@ -5,19 +5,19 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner (System.in);
 
-        System.out.println("Enter the customer Name : ");
+        System.out.print("Enter the customer Name : ");
         String customer_name = sc.nextLine();
 
-        System.out.println("Enter the movie name : ");
+        System.out.print("Enter the movie name : ");
         String movie = sc.nextLine();
 
-        System.out.println("Enter the number of tickets : ");
+        System.out.print("Enter the number of tickets : ");
         int tickets = sc.nextInt();
 
-        System.out.println("Enter the price of a ticket : ");
+        System.out.print("Enter the price of a ticket : ");
         double price = sc.nextDouble();
 
-        System.out.println("Enter your seat type : ");
+        System.out.print("Enter your seat type : ");
         char seat_type = sc.next().charAt(0);
 
         System.out.println(customer_name);
