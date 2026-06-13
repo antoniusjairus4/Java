17c34cd154f8e2b94c3837c2247f5f1c65edba15
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-06-13 11:45:59 +0530

    Adding packages

--- Basic_Problems/Student_data.java
+++ Basic_Problems/Student_data.java
@@ -7,6 +7,19 @@ public class Student_data {
 
         Scanner s = new Scanner(System.in);
 
+        System.out.printf("Enter your name : ");
         String name = s.nextLine();
+
+        System.out.printf("\n ENter your Reg No : ");
+        int reg = s.nextInt();
+
+        System.out.printf("\n Enter your SGPA of 2nd Sem");
+        int sgpa = s.nextInt();
+
+        System.out.println("Name : " + name);
+        System.out.println("Reg NO : " + reg);
+        System.out.println("SGPA : " + sgpa);
+
+        s.close();
     }
 }
