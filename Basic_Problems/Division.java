2355f1fc7cb7810ff9009a37ee8f72c73ee01a08
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-06-13 11:34:41 +0530

    Division

--- Basic_Problems/Division.java
+++ Basic_Problems/Division.java
@@ -5,5 +5,13 @@ import java.util.Scanner;
 public class Division {
     public static void main(String[] args) {
 
+        Scanner s = new Scanner(System.in);
+        
+        double num_1 = s.nextDouble();
+        double num_2 = s.nextDouble();
+
+        System.out.println(num_1 / num_2);
+
+        s.close();
     }
 }
