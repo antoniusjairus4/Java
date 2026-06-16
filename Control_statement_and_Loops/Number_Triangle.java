f4ac40e42361d486e85fba5009b170e79c0172da
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-06-16 19:24:29 +0530

    Printing the classic number triange

---
+++ Control_statement_and_Loops/Number_Triangle.java
@@ -0,0 +1,26 @@
+package Control_statement_and_Loops;
+
+import java.util.Scanner;
+
+class Number_Triangle{
+
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+
+        if(n < 2){
+            System.out.print("Invalid input");
+            return;
+        }
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.printf("\n");
+        }
+
+        s.close();
+    }
+}
