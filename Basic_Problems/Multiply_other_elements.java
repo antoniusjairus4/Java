bade0e09e77806f3fbbf656c590644cdab7cd6ed
antoniusjairus4 <antoniusjairusofficial@gmail.com>
2026-06-16 08:56:38 +0530

    To multiply all the elements in the array by skiping the element in that index

---
+++ Basic_Problems/Multiply_other_elements.java
@@ -0,0 +1,31 @@
+package Basic_Problems;
+
+import java.util.Scanner;
+
+class Multiply_other_elements{
+    public static void main(String[] args){
+        
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = s.nextInt();
+        }
+
+        for(int i = 0; i < n; i++){
+            int mul = 1;
+            for(int j = 0; j < n; j++){
+                if(i == j){
+                    continue;
+                }
+                else{
+                    mul = mul * arr[j];
+                }
+            }
+            System.out.print(mul + " ");
+        }
+
+        s.close();
+    }
+}
